947db159caab2db84e3f05f92841049495b71853
陳欣怡 <mandy.chen@hokia.com.tw>
2024-01-18 09:20:30 +0800

    feat：add path for nginx setting in app.js
    原因：新增 path 路徑及跨網域設定，以搭配 nginx 代理使用
    
    調整項目：
    1. app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const fastify = require('fastify');
 const http = require('http');
-// socket io api
+// socket io http api
 const socketIO = require('./src/socketIO');
 require('dotenv').config();
 
 const server = http.Server(fastify);
-const io = new socketIO(server);
+const io = new socketIO(server, {
+  cors: {
+    origin: ['http://192.168.25.180'], // nginx server ip
+  },
+  path: '/socket/socket.io', // socket io path
+});
 server.listen(process.env.SOCKET_IO_PORT, function (err) {
   console.log(
     `Welcome to socket io server listening at http://${process.env.IP_ADDRESS}:${process.env.SOCKET_IO_PORT}`,
