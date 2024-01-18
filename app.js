686c07d937d90469b2abc3ffd1187a4d859ba643
陳欣怡 <mandy.chen@hokia.com.tw>
2024-01-18 14:10:47 +0800

    feat：add ssl key and cert let socket io server can run under https in app.js
    原因：新增 ssl key 與 cert，讓 socket io server 走 https 協定
    
    調整項目：
    1. app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const fastify = require('fastify');
-const http = require('http');
-// socket io api
+const { readFileSync } = require('fs');
+const { createServer } = require('https');
+// socket io https api
 const socketIO = require('./src/socketIO');
 require('dotenv').config();
 
-const server = http.Server(fastify);
+// 將 fastify 放進 https 中開啟 Server 的 SOCKET_IO_PORT
+const server = createServer(
+  {
+    key: readFileSync('./src/ssl/eservice_hokia_com_tw.key'),
+    cert: readFileSync('./src/ssl/eservice_hokia_com_tw.crt'),
+  },
+  fastify,
+);
 const io = new socketIO(server);
 server.listen(process.env.SOCKET_IO_PORT, function (err) {
   console.log(
-    `Welcome to socket io server listening at http://${process.env.IP_ADDRESS}:${process.env.SOCKET_IO_PORT}`,
+    `Welcome to socket io server listening at https://${process.env.IP_ADDRESS}:${process.env.SOCKET_IO_PORT}`,
   );
   if (err) {
     console.log(err);
