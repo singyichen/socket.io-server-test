44c688751675e30ac73c1b7e37bd3609e22511b0
陳欣怡 <mandy.chen@hokia.com.tw>
2024-01-29 15:55:52 +0800

    feat：add cors setting in app.js
    原因：新增跨網域設定讓 api 可讓 nginx  server 拿取資料
    
    調整項目：
    1. app.js

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const { createServer } = require('https');
 // socket io https api
 const socketIO = require('./src/socketIO');
 require('dotenv').config();
-
 // 將 fastify 放進 https 中開啟 Server 的 SOCKET_IO_PORT
 const server = createServer(
   {
-    key: readFileSync('./src/ssl/eservice_hokia_com_tw.key'),
-    cert: readFileSync('./src/ssl/eservice_hokia_com_tw.crt'),
+    key: readFileSync('./src/ssl/192.168.25.180.key'),
+    cert: readFileSync('./src/ssl/192.168.25.180.crt'),
+    requestCert: false,
+    rejectUnauthorized: false,
   },
   fastify,
 );
-const io = new socketIO(server);
+const io = new socketIO(server, {
+  cors: {
+    origin: ['http://192.168.25.180', 'https://192.168.25.180'], // nginx server ip
+  },
+  path: '/socket/socket.io', // socket io path
+});
 server.listen(process.env.SOCKET_IO_PORT, function (err) {
   console.log(
     `Welcome to socket io server listening at https://${process.env.IP_ADDRESS}:${process.env.SOCKET_IO_PORT}`,
